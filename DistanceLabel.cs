c4470d0606a831638fb0e942b143cca4dfe56bb6
OwenPlamondon <yolocrazytrain@gmail.com>
2025-04-06 13:20:27 -0700

    Add files via upload

---
+++ DistanceLabel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceLabel : MonoBehaviour
+{
+    [SerializeField]
+    private PointA _pointAScript;
+    [SerializeField]
+    private GameObject _pointA;
+    [SerializeField]
+    private GameObject _pointB;
+    [SerializeField]
+    private GameObject _camera;
+    [SerializeField]
+    private TextMesh _text;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float midpointX = (_pointB.transform.position.x + _pointA.transform.position.x) / 2;
+        float midpointY = (_pointB.transform.position.y + 0.1f + _pointA.transform.position.y) / 2;
+        float midpointZ = (_pointB.transform.position.z + _pointA.transform.position.z) / 2;
+
+        this.transform.position = new Vector3(midpointX, midpointY, midpointZ);
+
+        _text.text = (_pointAScript._distance.ToString("F2")) + " meters";
+
+        Vector3 directionToCamera = _camera.transform.position - transform.position;
+        transform.LookAt(transform.position - directionToCamera);
+    }
+}
---
+++ PointA.cs
@@ -0,0 +1,43 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PointA : MonoBehaviour, IInputClickHandler
+{
+
+
+    [SerializeField]
+	private GameObject _pointB;
+	private float _distanceX;
+    private float _distanceY;
+    private float _distanceZ;
+    public float _distance;
+    private bool _following = false;
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        _distanceX = _pointB.transform.position.x - transform.position.x;
+        _distanceY = _pointB.transform.position.y - transform.position.y;
+        _distanceZ = _pointB.transform.position.z - transform.position.z;
+
+        _distance = Mathf.Sqrt(Mathf.Sqrt(Mathf.Pow(_distanceX, 2) + Mathf.Pow(_distanceY, 2)) + Mathf.Pow(_distanceZ, 2));
+    }
+
+    void IInputClickHandler.OnInputClicked(InputClickedEventData eventData)
+    {
+        if(_following == false)
+        {
+            this.transform.parent = GameObject.Find("Anchor_Cursor").transform;
+            _following = true;
+        }
+        else
+        {
+            this.transform.parent = null;
+            _following = false;
+        }
+    }
+}
